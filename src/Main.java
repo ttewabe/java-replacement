67fceb2736a76e7b0a3d2eea95fcea87c810caac
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-08-14 00:18:18 -0700

    Replacement

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "Hello World" + "!!";
+        System.out.println(message.replace("!", "*"));
+    }
+}
